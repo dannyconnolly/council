901113883aa03eb836f3a6c3d0541e353f9d63f6
Danny Connolly <dannyconnolly10@gmail.com>
2018-04-25 21:33:41 +0100

    Add channel filter dropdown vue component (#6)

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -29,17 +29,12 @@
                         <li><a href="/threads?unanswered=1">Unanswered Threads</a></li>
                     </ul>
                 </li>
-                <li><a href="/threads/create">New Thread</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Channels <span class="caret"></span>
-                    </a>
 
-                    <ul class="dropdown-menu" role="menu">
-                        @foreach ($channels as $channel)
-                            <li><a href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a></li>
-                        @endforeach
-                    </ul>
+                <li>
+                    <a href="/threads/create">New Thread</a>
                 </li>
+                
+                <channel-dropdown :channels="{{ $channels }}"></channel-dropdown>
 
             </ul>
 
@@ -47,14 +42,20 @@
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @guest
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
+                    <li>
+                        <a href="{{ route('login') }}">Login</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('register') }}">Register</a>
+                    </li>
                 @else
-                    
+
                     <user-notifications></user-notifications>
-                    
+
                     @if (Auth::user()->isAdmin())
-                        <li><a href="/admin"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span></a></li>
+                        <li>
+                            <a href="/admin"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span></a>
+                        </li>
                     @endif
 
                     <li class="dropdown">
@@ -63,11 +64,11 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ route('profile', Auth::user()) }}">My Profile</a></li>
                             <li>
-                                <a href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
+                                <a href="{{ route('profile', Auth::user()) }}">My Profile</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                                     Logout
                                 </a>
 
