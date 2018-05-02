7acc5068cc5ef4c294fc336cea90aeb5e2d5a9e7
dannyconnolly <daniel@tutza.com>
2018-05-02 22:20:11 +0100

    Change the look of the new reply section when viewing a thread. resolves #16

--- resources/assets/js/components/NewReply.vue
+++ resources/assets/js/components/NewReply.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="new-reply">
         <div v-if="signedIn">
             <div class="form-group">
                 <wysiwyg name="body" 
@@ -59,3 +59,11 @@
         }
     }
 </script>
+
+<style scoped>
+    .new-reply {
+        padding: 15px;
+        background-color: #fff;
+        border: 1px solid #e3e3e3;
+    }
+</style>
--- resources/assets/js/components/Wysiwyg.vue
+++ resources/assets/js/components/Wysiwyg.vue
@@ -22,3 +22,9 @@
         }
     }
 </script>
+
+<style scoped>
+    .trix-editor {
+        min-height: 100px;
+    }
+</style>
